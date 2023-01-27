bd1db0eea766e9e27be81eaa9232c4fbc8f13f5c
Yu-Nakaura <ailes.yuframboise@gmail.com>
2023-01-27 18:23:08 +0900

    changed image name

--- app/src/main/java/app/nakaura/chloe/school_checkwork_profile3/MainActivity.kt
+++ app/src/main/java/app/nakaura/chloe/school_checkwork_profile3/MainActivity.kt
@@ -1,7 +1,5 @@
 package app.nakaura.chloe.school_checkwork_profile3
 
-import android.content.res.ColorStateList
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import app.nakaura.chloe.school_checkwork_profile3.databinding.ActivityMainBinding
@@ -15,28 +13,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
 
-        //name
+        // 名前ボタンを押したとき
         binding.showProfileButton.setOnClickListener{
-            binding.profileImage.setImageResource(R.drawable.chloe_image)
+            binding.profileImage.setImageResource(R.drawable.profile_image)
             binding.profileLabelText.text = "名前"
             binding.profileCommentText.text = "Androidメンターのくろえだよ"
-            //color change
-            binding.showProfileButton.backgroundTintList =
-                ColorStateList.valueOf(Color.rgb(0, 180, 220))
-            binding.showFoodButton.backgroundTintList =
-                ColorStateList.valueOf(Color.rgb(150, 150, 150))
         }
 
-        //food
+        //　好きな食べ物ボタンを押したとき
         binding.showFoodButton.setOnClickListener{
-            binding.profileImage.setImageResource(R.drawable.korean_food_image)
+            binding.profileImage.setImageResource(R.drawable.food_image)
             binding.profileLabelText.text = "好きな食べ物"
             binding.profileCommentText.text = "辛い物が大好き"
-            //color change
-            binding.showProfileButton.backgroundTintList =
-                ColorStateList.valueOf(Color.rgb(150, 150, 150))
-            binding.showFoodButton.backgroundTintList =
-                ColorStateList.valueOf(Color.rgb(0, 180, 120))
         }
     }
 }
